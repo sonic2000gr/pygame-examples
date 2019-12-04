99e4fdd8b389d42db05cecfdea5392306c73a77b
Manolis Kiagias <sonic2000gr@gmail.com>
2019-12-04 07:23:20 +0200

    Add custom font to helloworld example

--- pygame-zero/Animation/helloworld.py
+++ pygame-zero/Animation/helloworld.py
@@ -4,4 +4,8 @@ TITLE = "Hello Pygame"
 
 def draw():
     screen.fill((50,80,250))
-    screen.draw.text("Hello World!", (30,50), fontsize = 64, color="red", background="yellow")
+    screen.draw.text("Hello World!", (30,50),
+                     fontname = "liberation-sans-regular",
+                     fontsize = 48,
+                     color="red",
+                     background="yellow")
